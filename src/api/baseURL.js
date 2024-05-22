3e5d69dfedd4a919a22ccbc8b0215ddd288ee1ec
Wondwessen <wondwosen.bewketu@kegeberewtech.com>
2024-05-22 02:12:55 -0700

    bug websocket route

--- src/api/baseURL.js
+++ src/api/baseURL.js
@@ -1,6 +1,11 @@
-export const BASE_URL = "https://kaps-api.purposeblacketh.com/";
+=
 //export const BASE_URL = "http://localhost:4000/";
 
+import socketIOClient from "socket.io-client";
+
+export const BASE_URL = "https://kaps-api.purposeblacketh.com/";
+
+export const socket = socketIOClient(BASE_URL);
 
 
 
